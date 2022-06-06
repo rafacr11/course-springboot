465336cec3c759953183c0afd34ea3d7aa2b7f43
Rafael De Carvalho  Ribeiro <rafael.ribeiro@inmetrics.com.br>
2022-06-06 17:28:35 -0300

    Order, Instant, ISO 8601 - Part 1

--- src/main/java/com/rafaelribeiro/course/entities/User.java
+++ src/main/java/com/rafaelribeiro/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.rafaelribeiro.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -90,6 +96,10 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
