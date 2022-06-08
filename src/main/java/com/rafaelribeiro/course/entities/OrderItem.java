834548533abd532519132d18617e209225db0f4e
Rafael De Carvalho  Ribeiro <rafael.ribeiro@inmetrics.com.br>
2022-06-08 11:07:01 -0300

    OrderItem, many-to-many association with extra attributes part two

--- src/main/java/com/rafaelribeiro/course/entities/OrderItem.java
+++ src/main/java/com/rafaelribeiro/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rafaelribeiro.course.entities.pk.OrderItemPK;
 
 @Entity
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -33,11 +34,12 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
 	
-	public void setOrder(Order order) {
+	public void setOrder(Order order) {	
 		id.setOrder(order);
 	}
 	
